410f171be4e719f4c1cc923f76210dbe50203b96
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-12 19:43:44 +0300

    update settings with heroku GDAL and GEOS dependencies

--- victorngeno/settings.py
+++ victorngeno/settings.py
@@ -134,4 +134,8 @@ if not DEBUG:
     db_from_env = dj_database_url.config()
     DATABASES = {'default': dj_database_url.config()}
     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+    
+    # HEROKU GDAL and GEOS settings for postgis above and for future blogs
+    GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+    GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
