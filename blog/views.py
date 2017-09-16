c81810eae895b1a3938f078c7eb30054cf777336
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-16 20:07:34 +0300

    create the feedback forms and confirm they are working

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from django.http import JsonResponse
 from django.core import mail
 from django.conf import settings
 
+
 def index(request):
-    template ="index.html"
+    template = "index.html"
     context = {
         "title":"Home"
     }
@@ -22,17 +23,15 @@ def contact(request):
             recipient_list=[settings.EMAIL_HOST_USER, 'ngenovictor321@gmail.com'],
             fail_silently=False,
             message=message,
-            html_message=message,
         )
         return JsonResponse({"message": "success"})
     else:
         return JsonResponse({"message": "error"})
 
 
-
 def view_blog_site(request):
     template = "blog.html"
     context = {
-        "title":"Veekay Blog"
+        "title": "Veekay Blog"
     }
     return render(request, template, context)
