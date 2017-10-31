1ac4fb7681a53312bc764e803161001005e318f6
ngenovictor <ngenovictor321@gmail.com>
2017-10-31 16:07:56 +0300

    added the url for journey

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="home"),
     url(r'^blog$', views.view_blog_site, name="blog"),
+    url(r'^journey$', views.journey, name="journey"),
     url(r'^contact', views.contact, name="contact")
 ]
