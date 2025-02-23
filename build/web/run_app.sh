b1a6921dde82968db7c371c76b8d95d6cbe44df5
victor ngeno <ngenovictor321@gmail.com>
2025-02-23 11:04:19 +0300

    run dev server on DEBGUD true

--- build/web/run_app.sh
+++ build/web/run_app.sh
@@ -8,6 +8,12 @@ python manage.py migrate
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-# Start Gunicorn processes
-echo "Starting Gunicorn."
-exec gunicorn --bind 0.0.0.0:8000 victorngeno.wsgi
+# if PORTFOLIO_BLOG_DEBUG is set to True, run the development server
+if [ "$PORTFOLIO_BLOG_DEBUG" = "True" ]; then
+    echo "Running development server"
+    python manage.py runserver 0.0.0.0:8000
+else
+    # Start Gunicorn processes
+    echo "Starting Gunicorn."
+    exec gunicorn --bind 0.0.0.0:8000 victorngeno.wsgi
+fi
