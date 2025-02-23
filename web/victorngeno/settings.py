40afd8e35389cb4ace082f3a088f3abf8164d39c
victor ngeno <ngenovictor321@gmail.com>
2025-02-23 10:47:32 +0300

    change static dir settings

--- web/victorngeno/settings.py
+++ web/victorngeno/settings.py
@@ -110,17 +110,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static", "our_static"),
+    os.path.join(
+        BASE_DIR,
+        "static",
+    ),
     # '/var/www/static/',
-    )
+)
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
 
 # email settings
