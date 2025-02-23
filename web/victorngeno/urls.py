ec0541775af7f65411f21e3bfa98470623ab867b
victor ngeno <ngenovictor321@gmail.com>
2025-02-23 10:48:28 +0300

    styling changes

--- web/victorngeno/urls.py
+++ web/victorngeno/urls.py
@@ -1,13 +1,16 @@
-from django.urls import include, path
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("blog.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
 # To load static files on deployment
 # if not settings.DEBUG:
